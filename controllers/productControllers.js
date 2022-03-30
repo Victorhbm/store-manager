ebda444c4743759e0362f6a110ebdac01f8bc5a3
victorhbm <victor.hugofla@hotmail.com>
2022-03-30 18:54:53 -0300

    feat(controllers): add controller to get product by id

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -6,10 +6,25 @@ const getAllProducts = async (_req, res) => {
 
     return res.status(200).json(products);
   } catch (error) {
-    return res.status(500).json({ message: 'Erro no servidor' });
+    return res.status(500).json({ message: 'Server error' });
+  }
+};
+
+const getProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { error, code, message, product } = await productServices.getProductById(+id);
+
+    const result = error ? { message } : product;
+
+    return res.status(code).json(result);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Server error' });
   }
 };
 
 module.exports = {
   getAllProducts,
+  getProductById,
 };
