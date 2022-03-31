edf23d0dcdda11356f739359b5ef06a20a07dd8f
victorhbm <victor.hugofla@hotmail.com>
2022-03-31 05:29:30 -0300

    feat(models): add model to get sale by id

--- models/saleModels.js
+++ models/saleModels.js
@@ -17,6 +17,27 @@ const getAllSales = async () => {
   return sales;
 };
 
+const getSaleById = async (id) => {
+  const [sale] = await connection.execute(
+    `
+    SELECT
+      s.date AS date,
+      sp.product_id AS productId,
+      sp.quantity AS quantity
+    FROM StoreManager.sales_products sp
+    INNER JOIN StoreManager.sales s
+    ON s.id = sp.sale_id
+    WHERE s.id = ?
+    `,
+    [id],
+  );
+
+  if (sale.length === 0) return null;
+
+  return sale;
+};
+
 module.exports = {
   getAllSales,
+  getSaleById,
 };
