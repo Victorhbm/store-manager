0095f62f56b776b077835423f90045f1d8f73342
victorhbm <victor.hugofla@hotmail.com>
2022-03-31 05:30:24 -0300

    feat(controllers): add controller to get sale by id

--- controllers/saleControllers.js
+++ controllers/saleControllers.js
@@ -10,6 +10,21 @@ const getAllSales = async (_req, res) => {
   }
 };
 
+const getSaleById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { error, code, message, sale } = await saleServices.getSaleById(+id);
+
+    const result = error ? { message } : sale;
+
+    return res.status(code).json(result);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Server error' });
+  }
+};
+
 module.exports = {
   getAllSales,
+  getSaleById,
 };
