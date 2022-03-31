0ebfeedf9eab979e4843e21839c93c22fa997abb
victorhbm <victor.hugofla@hotmail.com>
2022-03-31 05:29:51 -0300

    feat(services): add service to get sale by id

--- services/saleServices.js
+++ services/saleServices.js
@@ -6,6 +6,15 @@ const getAllSales = async () => {
   return sales;
 };
 
+const getSaleById = async (id) => {
+  const sale = await saleModels.getSaleById(id);
+
+  if (!sale) return { error: true, code: 404, message: 'Sale not found' };
+
+  return { error: false, code: 200, sale };
+};
+
 module.exports = {
   getAllSales,
+  getSaleById,
 };
