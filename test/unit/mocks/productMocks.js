5dae3826fe8b38930728a1f8f0ee204e86ed43fb
victorhbm <victor.hugofla@hotmail.com>
2022-03-31 17:02:56 -0300

    test(mocks/productMocks): create product mocks

---
+++ test/unit/mocks/productMocks.js
@@ -0,0 +1,37 @@
+const fakeProducts = [
+  {
+    id: 1,
+    name: "Martelo de Thor",
+    quantity: 10
+  },
+  {
+    id: 2,
+    name: "Traje de encolhimento",
+    quantity: 20
+  },
+  {
+    id: 3,
+    name: "Escudo do Capitão América",
+    quantity: 30
+  }
+];
+
+const fakeProduct = [
+  {
+    id: 1,
+    name: "Martelo de Thor",
+    quantity: 10
+  }
+];
+
+const fakeProductObj = {
+  id: 1,
+  name: "Martelo de Thor",
+  quantity: 10
+}
+
+module.exports = {
+  fakeProducts,
+  fakeProduct,
+  fakeProductObj,
+}
