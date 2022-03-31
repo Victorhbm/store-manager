5f2327f07f68241e5f0ea62b3ed203222bf5e156
victorhbm <victor.hugofla@hotmail.com>
2022-03-31 11:33:10 -0300

    feat(controllers/saleControllers): add controller to create a sale

--- controllers/saleControllers.js
+++ controllers/saleControllers.js
@@ -1,12 +1,15 @@
 const saleServices = require('../services/saleServices');
 
+const MESSAGE_ERROR = 'Server error';
+const CODE_ISR = 500;
+
 const getAllSales = async (_req, res) => {
   try {
     const sales = await saleServices.getAllSales();
 
     return res.status(200).json(sales);
   } catch (error) {
-    return res.status(500).json({ message: 'Server error' });
+    return res.status(CODE_ISR).json({ message: MESSAGE_ERROR });
   }
 };
 
@@ -20,11 +23,23 @@ const getSaleById = async (req, res) => {
     return res.status(code).json(result);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: 'Server error' });
+    return res.status(CODE_ISR).json({ message: MESSAGE_ERROR });
+  }
+};
+
+const createSale = async (req, res) => {
+  try {
+    const createdProduct = await saleServices.createSale(req.body);
+
+    return res.status(201).json(createdProduct);
+  } catch (error) {
+    console.error(error);
+    return res.status(CODE_ISR).json({ message: MESSAGE_ERROR });
   }
 };
 
 module.exports = {
   getAllSales,
   getSaleById,
+  createSale,
 };
