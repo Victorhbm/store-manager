6ed9f6c8f0361796a94dd42368a2f4f3885aa91f
victorhbm <victor.hugofla@hotmail.com>
2022-04-01 19:16:58 -0300

    feat(middlewares/productMiddlewares): create middleware to validate products amount

--- middlewares/saleMiddlewares.js
+++ middlewares/saleMiddlewares.js
@@ -1,4 +1,4 @@
-const { validateAllProducts } = require('../schemas/saleSchema');
+const { validateAllProducts, validateAmount } = require('../schemas/saleSchema');
 
 function validateSale(req, res, next) {
   const { code, message } = validateAllProducts(req.body);
@@ -8,6 +8,19 @@ function validateSale(req, res, next) {
   next();
 }
 
+async function validateProductsAmount(req, res, next) {
+  const validation = await validateAmount(req.body);
+
+  if (!validation) {
+    return res.status(422).json({
+      message: 'Such amount is not permitted to sell',
+    });
+  }
+
+  next();
+}
+
 module.exports = {
   validateSale,
+  validateProductsAmount,
 };
