e8e229415239b9ea9a4bacefe0e05764cfe12f58
victorhbm <victor.hugofla@hotmail.com>
2022-04-02 08:49:32 -0300

    feat(routes): create router to sales

---
+++ routes/salesRouter.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { validateSale, validateProductsAmount } = require('../middlewares/saleMiddlewares');
+const saleControllers = require('../controllers/saleControllers');
+
+const router = express.Router();
+
+router.get('/', saleControllers.getAllSales);
+
+router.get('/:id', saleControllers.getSaleById);
+
+router.post(
+  '/',
+  validateSale,
+  validateProductsAmount,
+  saleControllers.createSale,
+);
+
+router.put(
+  '/:id',
+  validateSale,
+  saleControllers.updateSale,
+);
+
+router.delete(
+  '/:id',
+  saleControllers.deleteSale,
+);
+
+module.exports = router;
