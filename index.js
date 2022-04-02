bcf9a51cd677814b1618fa7cd46e2e2e5c081bb0
victorhbm <victor.hugofla@hotmail.com>
2022-04-02 08:50:12 -0300

    refactor(index.js): change endpoints to use routes

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const productControllers = require('./controllers/productControllers');
-const { validateProduct, validateName } = require('./middlewares/productMiddlewares');
-const { validateSale, validateProductsAmount } = require('./middlewares/saleMiddlewares');
-const saleControllers = require('./controllers/saleControllers');
+const productsRouter = require('./routes/productsRouter');
+const salesRouter = require('./routes/salesRouter');
 
 const app = express();
 
@@ -14,41 +12,8 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
-app.get('/products', productControllers.getAllProducts);
-app.get('/products/:id', productControllers.getProductById);
-app.post(
-  '/products',
-  validateProduct,
-  validateName,
-  productControllers.createProduct,
-);
-app.put(
-  '/products/:id',
-  validateProduct,
-  validateName,
-  productControllers.updateProduct,
-);
-app.delete(
-  '/products/:id',
-  productControllers.deleteProduct,
-);
-app.get('/sales', saleControllers.getAllSales);
-app.get('/sales/:id', saleControllers.getSaleById);
-app.post(
-  '/sales',
-  validateSale,
-  validateProductsAmount,
-  saleControllers.createSale,
-);
-app.put(
-  '/sales/:id',
-  validateSale,
-  saleControllers.updateSale,
-);
-app.delete(
-  '/sales/:id',
-  saleControllers.deleteSale,
-);
+app.use('/products', productsRouter);
+app.use('/sales', salesRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
