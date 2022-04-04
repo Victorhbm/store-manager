034f2740dbb3656be281b8f35ecdced901b0224d
victorhbm <victor.hugofla@hotmail.com>
2022-04-04 12:39:01 -0300

    test(services/sales): fixes createSale and deleteSale service tests

--- test/unit/services/sales.js
+++ test/unit/services/sales.js
@@ -3,12 +3,18 @@ const { describe } = require("mocha");
 const sinon = require("sinon");
 const saleModels = require("../../../models/saleModels");
 const saleServices = require("../../../services/saleServices");
-const { fakeSales, fakeSalesById, fakeSaleProducts, fakeSaleReturn } = require('../mocks/saleMocks');
+const saleSchema = require('../../../schemas/saleSchema');
+const {
+  fakeSales,
+  fakeSalesById,
+  fakeSaleProducts,
+  fakeSaleReturn,
+} = require("../mocks/saleMocks");
 
-describe('Executa a service getAllSales', () => {
-  describe('verifica se a funcao', () => {
-    it('retorna os dados corretos', async () => {
-      sinon.stub(saleModels, 'getAllSales').resolves(fakeSales);
+describe("Executa a service getAllSales", () => {
+  describe("verifica se a funcao", () => {
+    it("retorna os dados corretos", async () => {
+      sinon.stub(saleModels, "getAllSales").resolves(fakeSales);
 
       const result = await saleServices.getAllSales();
 
@@ -19,14 +25,14 @@ describe('Executa a service getAllSales', () => {
   });
 });
 
-describe('Executa a service getSaleById', () => {
-  describe('verifica se a funcao', () => {
-    describe('retorna os dados corretos ao', () => {
-      it('passar um ID existente', async () => {
-        sinon.stub(saleModels, 'getSaleById').resolves(fakeSalesById);
+describe("Executa a service getSaleById", () => {
+  describe("verifica se a funcao", () => {
+    describe("retorna os dados corretos ao", () => {
+      it("passar um ID existente", async () => {
+        sinon.stub(saleModels, "getSaleById").resolves(fakeSalesById);
 
         const result = await saleServices.getSaleById(1);
-        
+
         expect(result.error).to.be.false;
         expect(result.code).to.be.equals(200);
         expect(result.sale).to.be.equals(fakeSalesById);
@@ -34,62 +40,69 @@ describe('Executa a service getSaleById', () => {
         saleModels.getSaleById.restore();
       });
 
-      it('passar um ID inexistente', async () => {
-        sinon.stub(saleModels, 'getSaleById').resolves(null);
+      it("passar um ID inexistente", async () => {
+        sinon.stub(saleModels, "getSaleById").resolves(null);
 
         const result = await saleServices.getSaleById(25);
-  
+
         expect(result.error).to.be.true;
         expect(result.code).to.be.equals(404);
-        expect(result.message).to.be.equals('Sale not found');
+        expect(result.message).to.be.equals("Sale not found");
 
         saleModels.getSaleById.restore();
       });
-    })
+    });
   });
 });
 
-describe('Executa a service createSale', () => {
-  describe('verifica se a funcao', () => {
-    it('retorna os dados corretos', async () => {
-      sinon.stub(saleModels, 'createSale').resolves(fakeSaleReturn);
+describe("Executa a service createSale", () => {
+  describe("verifica se a funcao", () => {
+    it("retorna os dados corretos", async () => {
+      sinon.stub(saleModels, "createSale").resolves(fakeSaleReturn);
+      sinon.stub(saleSchema, "updateProductsQuantity").resolves();
 
       const result = await saleServices.createSale(fakeSaleProducts);
-      
+
       expect(result.id).to.be.equals(1);
       expect(result.itemsSold[0].productId).to.be.equals(1);
       expect(result.itemsSold[0].quantity).to.be.equals(2);
       expect(result.itemsSold[1].productId).to.be.equals(2);
       expect(result.itemsSold[1].quantity).to.be.equals(5);
+      expect(saleSchema.updateProductsQuantity.calledWith(fakeSaleProducts)).to.be.true;
 
       saleModels.createSale.restore();
+      saleSchema.updateProductsQuantity.restore();
     });
   });
 });
 
-describe('Executa a service deleteSale', () => {
-  describe('verifica se a funcao retorna os dados corretos ao', () => {
-    it('passar um ID existente', async () => {
-      sinon.stub(saleModels, 'getSaleById').resolves(fakeSalesById);
-      sinon.stub(saleModels, 'deleteSale').resolves();
+describe("Executa a service deleteSale", () => {
+  describe("verifica se a funcao retorna os dados corretos ao", () => {
+    it("passar um ID existente", async () => {
+      sinon.stub(saleModels, "getSaleById").resolves(fakeSalesById);
+      sinon.stub(saleModels, "deleteSale").resolves();
+      sinon.stub(saleSchema, "updateProductsOnDelete").resolves();
 
       const result = await saleServices.deleteSale(1);
 
       expect(result.error).to.be.false;
       expect(result.code).to.be.equals(204);
+      expect(saleModels.deleteSale.calledWith(1)).to.be.true;
+      expect(saleSchema.updateProductsOnDelete.calledWith(1)).to.be.true;
 
       saleModels.getSaleById.restore();
       saleModels.deleteSale.restore();
+      saleSchema.updateProductsOnDelete.restore();
     });
 
-    it('passar um ID inexistente', async () => {
-      sinon.stub(saleModels, 'getSaleById').resolves(null);
+    it("passar um ID inexistente", async () => {
+      sinon.stub(saleModels, "getSaleById").resolves(null);
 
       const result = await saleServices.deleteSale(25);
 
       expect(result.error).to.be.true;
       expect(result.code).to.be.equals(404);
-      expect(result.message).to.be.equals('Sale not found');
+      expect(result.message).to.be.equals("Sale not found");
 
       saleModels.getSaleById.restore();
     });
